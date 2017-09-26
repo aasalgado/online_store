42539130099f095cdfa618ccde916f9364298698
Anthony Salgado <anthonyasalgado@gmail.com>
2017-09-26 16:50:17 -0700

    added order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925190008) do
+ActiveRecord::Schema.define(version: 20170926233300) do
 
   create_table "merchandises", force: :cascade do |t|
     t.string "category"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170925190008) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id_id"
+    t.integer "merchandise_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["merchandise_id_id"], name: "index_orders_on_merchandise_id_id"
+    t.index ["user_id_id"], name: "index_orders_on_user_id_id"
   end
 
   create_table "users", force: :cascade do |t|
